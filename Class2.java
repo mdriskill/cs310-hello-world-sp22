8b874a8373bc7f3ffd80001d8cc4ce8f1e95e0d6
Mason Driskill <mdriskill@stu.jsu.edu>
2022-01-29 15:50:46 -0800

    Started f4()

--- Class2.java
+++ Class2.java
@@ -14,5 +14,8 @@ public class Class2 {
      private void f3() {
         // v3.0
     }
+     private void f4() {
+        // v4.0(in progress)
+    }
 
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -19,5 +19,8 @@ public class HelloWorld {
      private void f3() {
         // v3.0
     }
+     private void f4() {
+        // v4.0(in progress)
+    }
 
 }
